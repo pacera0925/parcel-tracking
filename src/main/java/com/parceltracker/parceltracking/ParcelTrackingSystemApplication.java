43e65691f51b49f2646d867832b68135d469af80
Paul Cera <paul@edusuite.asia>
2024-06-19 19:36:22 +0800

    Initial commit

---
+++ src/main/java/com/parceltracker/parceltracking/ParcelTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.parceltracker.parceltracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parceltracker/parceltracking/ParcelTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parceltracker.parceltracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
